57852b928fea53a471f3c79b3b0a3d540206c16c
IS-Academy <choiis.academy@gmail.com>
2025-03-31 14:59:06 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // config.js
-module.exports = {
-  TELEGRAM_BOT_TOKEN: '7772330870:AAHDLP4xhhQzgl-nBxlXGj86pb9dVVHfKTg',
-  TELEGRAM_CHAT_ID: '-1002567183449'
-};
+require('dotenv').config();
 
+module.exports = {
+  TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+  TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID,
+};
