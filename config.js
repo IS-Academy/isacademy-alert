0fdda53b9ff29ebd5c6070daa3482eebf37477a1
IS-Academy <choiis.academy@gmail.com>
2025-03-31 22:42:57 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,11 @@
 require('dotenv').config();
 
 module.exports = {
+  // 최실장 봇
   TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
   TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID,
+
+  // 밍밍 봇
+  TELEGRAM_BOT_TOKEN_A: process.env.TELEGRAM_BOT_TOKEN_A,
+  TELEGRAM_CHAT_ID_A: process.env.TELEGRAM_CHAT_ID_A,  
 };
