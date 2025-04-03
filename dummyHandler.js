a0502bcf69f927cff55df1a1e8bb1f12fdcb36ff
IS-Academy <choiis.academy@gmail.com>
2025-04-03 20:45:29 +0900

    Update dummyHandler.js

--- dummyHandler.js
+++ dummyHandler.js
@@ -1,8 +1,12 @@
-// dummyHandler.js
 const { sendTextToTelegram } = require('./utils');
+const fs = require('fs');
 
 module.exports = async function dummyHandler(req, res) {
   const now = new Date().toLocaleString('ko-KR', { timeZone: 'Asia/Seoul' });
+
+  // 마지막 더미 수신 시간 저장
+  fs.writeFileSync('./last_dummy.txt', now);
+
   const msg = `✅ 더미 신호 수신됨\n⏰ 시각: ${now}`;
   await sendTextToTelegram(msg);
   console.log('[DUMMY] 알림 수신됨:', now);
