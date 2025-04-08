93de3082844dc2d22ad0dc4db50b73fa041fb608
IS-Academy <choiis.academy@gmail.com>
2025-04-08 13:54:04 +0900

    Update setlang.js

--- commands/setlang.js
+++ commands/setlang.js
@@ -1,18 +1,21 @@
-// commands/setlang.js
-const { getReplyKeyboard, sendTextToTelegram } = require('../utils');
-const langMessages = require('../langMessages');
-const langManager = require('../langConfigManager');
+// ✅ setlang.js - 언어 설정 후 상태 메시지 갱신
 
-module.exports = async function handleSetLangCommand(chatId, input, lang, timeStr) {
-  if (!input) {
-    await sendTextToTelegram('🌐 언어를 선택해주세요:', getReplyKeyboard('lang'));
+const langManager = require('../langConfigManager');
+const { sendToAdmin } = require('../botManager');
+const { getTimeString } = require('../utils');
+const sendBotStatus = require('./status');
+const config = require('../config');
+
+module.exports = async function handleSetLang(chatId, langCode, currentLang, timeStr) {
+  if (!langCode || !['ko', 'en', 'zh', 'ja'].includes(langCode)) {
+    await sendToAdmin('❗ 올바른 언어 코드를 입력하세요. (ko, en, zh, ja)');
     return;
   }
 
-  const success = langManager.setUserLang(chatId, input);
-  const msg = success
-    ? langMessages.setLangSuccess[lang](input)
-    : langMessages.setLangFail[lang]();
-
-  await sendTextToTelegram(`${msg} (🕒 ${timeStr})`);
+  const success = langManager.setUserLang(chatId, langCode);
+  if (success) {
+    await sendBotStatus(getTimeString(), '', chatId); // 상태 메시지 갱신
+  } else {
+    await sendToAdmin('❌ 언어 설정 실패');
+  }
 };
