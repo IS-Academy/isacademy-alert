8e112d927f4970be17a13cb0f197d76f73e3f320
IS-Academy <choiis.academy@gmail.com>
2025-04-08 23:01:53 +0900

    Update dummyHandler.js

--- dummyHandler.js
+++ dummyHandler.js
@@ -1,17 +1,21 @@
 // ✅ dummyHandler.js
 
-const { updateLastDummyTime, sendTextToTelegram } = require('./utils');
 const moment = require('moment-timezone');
 const config = require('./config');
+const { updateLastDummyTime, getTimeString } = require('./utils');
+const sendBotStatus = require('./commands/status');
 
 module.exports = async function dummyHandler(req, res) {
-  const now = moment().tz(config.DEFAULT_TIMEZONE).format('YYYY.MM.DD HH:mm:ss');
+  const now = moment().tz(config.DEFAULT_TIMEZONE).format('YY.MM.DD HH:mm:ss');
 
   // 콘솔 기록 및 변수 저장
   console.log('✅ [더미 수신] 시간:', now);
   updateLastDummyTime(now);
 
-  // 필요 시 관리자에게 메시지 발송 (주석 해제 가능)
+  // ✅ 관리자봇 상태 자동 갱신 (더미 수신 시)
+  await sendBotStatus(getTimeString());
+
+  // ✅ 필요 시 관리자에게 메시지 발송 (주석 해제 가능)
   // await sendTextToTelegram(`🔁 더미 웹훅 수신!\n🕒 ${now}`);
 
   res.status(200).send('✅ 더미 수신 완료');
