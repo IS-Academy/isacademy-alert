fba28144ac4d7f6afd8488426b3119976dd93db3
IS-Academy <choiis.academy@gmail.com>
2025-04-09 09:26:31 +0900

    Create jp.js

---
+++ locales/jp.js
@@ -0,0 +1,12 @@
+module.exports = {
+  symbol: "シンボル",
+  timeframe: "時間足",
+  price: "価格",
+  entry: "エントリー",
+  avgPrice: "平均価格",
+  profit: "収益率",
+  roe: "自己資金比",
+  capturedAt: "検出時間",
+  notice1: "⚠️観点共有は任意参加です。",
+  notice2: "⚠️すべての投資判断はご自身の責任でお願いします。"
+};
