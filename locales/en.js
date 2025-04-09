b91002959d4c1a70d116138cf67c1a23ca05e318
IS-Academy <choiis.academy@gmail.com>
2025-04-09 09:26:07 +0900

    Create en.js

---
+++ locales/en.js
@@ -0,0 +1,12 @@
+module.exports = {
+  symbol: "Symbol",
+  timeframe: "Timeframe",
+  price: "Price",
+  entry: "Entry",
+  avgPrice: "Avg. Price",
+  profit: "PnL",
+  roe: "ROE",
+  capturedAt: "Captured At",
+  notice1: "⚠️Signals are always optional.",
+  notice2: "⚠️All investment decisions are your own responsibility."
+};
