c007e786362afd6bbdff3da49aed9b4f06d83168
IS-Academy <choiis.academy@gmail.com>
2025-04-09 09:37:10 +0900

    Create testMessage.js

---
+++ testMessage.js
@@ -0,0 +1,19 @@
+// testMessage.js
+const { formatSignalMessage } = require('./handlers/messageTemplateManager');
+
+const testData = {
+  symbol: 'BTCUSDT.P',
+  timeframe: '5min',
+  price: '86365.40',
+  entry: { percent: 1, avgPrice: '75138.70' },
+  result: { pnl: '+14.95%', roe: '+0.74%' },
+  time: '2025. 04. 09. (수)\n오전 09:00:00'
+};
+
+const langs = ['ko', 'en', 'zh', 'jp'];
+
+langs.forEach(lang => {
+  const message = formatSignalMessage('exitShort', testData, lang);
+  console.log(`\n===== [${lang.toUpperCase()}] 메시지 출력 =====\n`);
+  console.log(message);
+});
