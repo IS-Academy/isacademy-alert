63597ed7bc85318004f60915d847083ab5723ba9
IS-Academy <choiis.academy@gmail.com>
2025-04-09 09:34:46 +0900

    Add files via upload

---
+++ locales/zh.js
@@ -0,0 +1,39 @@
+module.exports = {
+  symbols: {
+    Ready_showSup: "#🩵做多准备 📈",
+    Ready_showRes: "#❤️做空准备 📉",
+    Ready_isBigSup: "#🚀强烈做多准备 📈",
+    Ready_isBigRes: "#🛸强烈做空准备 📉",
+    Ready_exitLong: "#💲平多准备 📈",
+    Ready_exitShort: "#💲平空准备 📉",
+    showSup: "#🩵做多进场 📈 观点分享🩵",
+    showRes: "#❤️做空进场 📉 观点分享❤️",
+    isBigSup: "#🚀强烈做多进场 📈 观点分享🚀",
+    isBigRes: "#🛸强烈做空进场 📉 观点分享🛸",
+    exitLong: "#💰平多 📈 观点分享💰",
+    exitShort: "#💰平空 📉 观点分享💰"
+  },
+  labels: {
+    symbol: "📌 币种",
+    timeframe: "⏱️ 周期",
+    price: "💲 价格",
+    entryInfo: "📊 已进场 {entryCount}% / 均价 {entryAvg}",
+    entrySummary: "⏱️ 入场概况:",
+    entryInfoByTF: "• {tf}分钟 → ✅ {percent}% / 均价 {avg}",
+    pnlLineProfit: "📈收益率 {pnl}% / 本金比 {capital}%",
+    pnlLineLoss: "📉亏损率 {pnl}% / 本金比 {capital}%",
+    entryLimitReachedLong: "⚠️ 多头持仓已饱和。",
+    entryLimitReachedShort: "⚠️ 空头持仓已饱和。",
+    captured: "🕒 捕捉时间",
+    weight: "🗝️ 仓位: {weight}",
+    leverage: "🎲 杠杆: {leverage}",
+    pnl: "📈 盈亏: {value}%",
+    disclaimer_short: "⚠️观点为自愿参与。",
+    disclaimer_full: "⚠️观点分享纯属自愿\n⚠️所有交易和决策需自行承担。"
+  },
+  days: {
+    Mon: "周一", Tue: "周二", Wed: "周三", Thu: "周四", Fri: "周五", Sat: "周六", Sun: "周日"
+  },
+  am: "上午",
+  pm: "下午"
+};
