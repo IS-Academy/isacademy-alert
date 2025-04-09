a037a5ab2164fb21c990b36727aaef7e64838670
IS-Academy <choiis.academy@gmail.com>
2025-04-09 11:18:37 +0900

    Add files via upload

--- locales/en.js
+++ locales/en.js
@@ -1,39 +1,14 @@
 module.exports = {
-  symbols: {
-    Ready_showSup: "#🩵Long Setup 📈",
-    Ready_showRes: "#❤️Short Setup 📉",
-    Ready_isBigSup: "#🚀Strong Long Setup 📈",
-    Ready_isBigRes: "#🛸Strong Short Setup 📉",
-    Ready_exitLong: "#💲Exit Long Ready 📈",
-    Ready_exitShort: "#💲Exit Short Ready 📉",
-    showSup: "#🩵Long Entry 📈 Perspective🩵",
-    showRes: "#❤️Short Entry📉Perspective❤️",
-    isBigSup: "#🚀Strong Long Entry 📈 Perspective🚀",
-    isBigRes: "#🛸Strong Short Entry 📉 Perspective🛸",
-    exitLong: "#💰Exit Long 📈 Perspective💰",
-    exitShort: "#💰Exit Short 📉 Perspective💰"
-  },
   labels: {
-    symbol: "📌 Symbol",
-    timeframe: "⏱️ Timeframe",
-    price: "💲 Price",
-    entryInfo: "📊 Entry {entryCount}% / Avg {entryAvg}",
-    entrySummary: "⏱️ Entry Summary:",
-    entryInfoByTF: "• {tf}min → ✅ {percent}% / Avg {avg}",
-    pnlLineProfit: "📈PnL {pnl}% / Capital {capital}%",
-    pnlLineLoss: "📉PnL {pnl}% / Capital {capital}%",
-    entryLimitReachedLong: "⚠️ Long position is saturated.",
-    entryLimitReachedShort: "⚠️ Short position is saturated.",
-    captured: "🕒 Captured At",
-    weight: "🗝️ Weight: {weight}",
-    leverage: "🎲 Leverage: {leverage}",
-    pnl: "📈 PnL: {value}%",
-    disclaimer_short: "⚠️This view is voluntary.",
-    disclaimer_full: "⚠️Participation is always voluntary.\n⚠️All decisions are your own responsibility."
-  },
-  days: {
-    Mon: "Mon", Tue: "Tue", Wed: "Wed", Thu: "Thu", Fri: "Fri", Sat: "Sat", Sun: "Sun"
-  },
-  am: "AM",
-  pm: "PM"
+  "symbol": "📌 Symbol",
+  "timeframe": "⏱️ Timeframe",
+  "price": "💲 Price",
+  "entry": "Entry",
+  "avgPrice": "Avg. Price",
+  "profit": "PnL",
+  "roe": "ROE",
+  "capturedAt": "🕒 Captured at",
+  "notice1": "⚠️Signals are always optional.",
+  "notice2": "⚠️All investment decisions are your own responsibility."
+}
 };
