290861bccf3d7b75c39fa1acaf1c61fb308cf951
IS-Academy <choiis.academy@gmail.com>
2025-04-10 15:21:23 +0900

    Update utils.js

--- utils.js
+++ utils.js
@@ -16,20 +16,18 @@ function saveBotState(state) {
   fs.writeFileSync('./bot_state.json', JSON.stringify(state, null, 2));
 }
 
-// 추가 (복원된 코드)
 function loadBotState() {
   try {
-    const raw = fs.readFileSync('./bot_state.json');
-    return JSON.parse(raw);
-  } catch {
+    return JSON.parse(fs.readFileSync('./bot_state.json'));
+  } catch (err) {
     return { choiEnabled: true, mingEnabled: true };
   }
 }
 
-module.exports = {
-  setAdminMessageId,
-  getAdminMessageId,
-  getTimeString,
+module.exports = { 
+  setAdminMessageId, 
+  getAdminMessageId, 
+  getTimeString, 
   saveBotState,
-  loadBotState, // ✅ 추가
+  loadBotState
 };
