3c57aea38bac1a2272a730beafa13b22232795a7
IS-Academy <choiis.academy@gmail.com>
2025-04-10 12:23:19 +0900

    Update dummyHandler.js

--- dummyHandler.js
+++ dummyHandler.js
@@ -1,6 +1,6 @@
-// ✅ dummyHandler.js
+// ✅👇 dummyHandler.js
 
-const { updateLastDummyTime, getLastDummyTime } = require('./utils');
+const { updateLastDummyTime, getAdminMessageId } = require('./utils');
 const sendBotStatus = require('./commands/status');
 const moment = require('moment-timezone');
 const config = require('./config');
@@ -11,9 +11,14 @@ module.exports = async function dummyHandler(req, res) {
 
   const timeStr = moment().tz(config.DEFAULT_TIMEZONE).format('YY.MM.DD HH:mm:ss');
 
-  // 👇 인라인 키보드 없이 기존 메시지를 업데이트 (메시지 수정 방식으로 동작)
-  await sendBotStatus(timeStr, 'dummy', config.ADMIN_CHAT_ID);
+  // 기존 메시지의 messageId로 수정
+  const adminMessageId = getAdminMessageId();
+
+  if (adminMessageId) {
+    await sendBotStatus(timeStr, 'dummy', config.ADMIN_CHAT_ID, adminMessageId);
+  } else {
+    console.warn('⚠️ 저장된 adminMessageId가 없음, 최초 메시지 생성 필요');
+  }
 
   res.status(200).send('✅ 더미 수신 완료');
 };
-
