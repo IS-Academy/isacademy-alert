131e4f8b5525aa789822eabe233d6bf8e50522e9
IS-Academy <choiis.academy@gmail.com>
2025-04-12 17:57:39 +0900

    Update webhookHandler.js

--- webhookHandler.js
+++ webhookHandler.js
@@ -104,7 +104,7 @@ module.exports = async function webhookHandler(req, res) {
       global.choiEnabled = cmd === "choi_on" ? true : cmd === "choi_off" ? false : global.choiEnabled;
       global.mingEnabled = cmd === "ming_on" ? true : cmd === "ming_off" ? false : global.mingEnabled;
       saveBotState({ choiEnabled: global.choiEnabled, mingEnabled: global.mingEnabled });
-    } else if (cmd.startsWith("lang_")) {
+    } else if (cmd.startsWith("lang_choi_") || cmd.startsWith("lang_ming_")) {
       const [_, bot, langCode] = cmd.split("_");
       const targetId = bot === "choi" ? config.TELEGRAM_CHAT_ID : config.TELEGRAM_CHAT_ID_A;
       langManager.setUserLang(targetId, langCode);
