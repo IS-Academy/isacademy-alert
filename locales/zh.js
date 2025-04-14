b329f32414d961880248ece1699c499fe53d0970
IS-Academy <choiis.academy@gmail.com>
2025-04-14 09:59:03 +0900

    Update zh.js

--- locales/zh.js
+++ locales/zh.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   labels: {
     symbol: "📌 币种",
-    timeframe: "⏱️ 周期",
+    timeframe: "⏱️ 周期" {interval}分钟,
     price: "💲 价格",
     entryInfo: "📊 已进场 {entryCount}% / 均价 {entryAvg}",
     entrySummary: "⏱️ 入场概况:",
