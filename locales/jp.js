be1f5a2cfdb62304c526deb106fbb1bd5f84977c
IS-Academy <choiis.academy@gmail.com>
2025-04-14 10:57:33 +0900

    Update jp.js

--- locales/jp.js
+++ locales/jp.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   labels: {
     symbol: "📌 シンボル",
-    timeframe: "⏱️ 時間枠 {interval}分",
+    timeframe: "⏱️ 時間枠",
     price: "💲 価格",
     entryInfo: "📊 エントリー {entryCount}% / 平均価格 {entryAvg}",
     entrySummary: "⏱️ エントリー状況:",
