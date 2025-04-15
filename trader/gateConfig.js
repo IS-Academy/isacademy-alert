76ba0949efff5c5b5590b8d300265cbed434a263
IS-Academy <choiis.academy@gmail.com>
2025-04-15 09:30:00 +0900

    Create gateConfig.js

---
+++ trader/gateConfig.js
@@ -0,0 +1,9 @@
+// ✅ /trader/gateConfig.js
+
+module.exports = {
+  GATE_API_KEY: 'YOUR_GATE_IO_API_KEY_HERE',
+  GATE_API_SECRET: 'YOUR_GATE_IO_SECRET_HERE',
+  BASE_URL: 'https://api.gate.io/api/v4',
+  DEFAULT_PAIR: 'BTC_USDT',
+  DEFAULT_AMOUNT: 0.001
+};
