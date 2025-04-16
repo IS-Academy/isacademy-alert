58c682a85af7c26f04c9155df603f6df5e671a0c
IS-Academy <choiis.academy@gmail.com>
2025-04-16 22:19:02 +0900

    Update en.js

--- locales/en.js
+++ locales/en.js
@@ -21,6 +21,8 @@ module.exports = {
     timeframeUnit: "min",
     price: "💲 Price",
     entryInfo: "📊 Entry {entryCount}% / Avg {entryAvg}",
+    noEntryInfo: "📊 No entry ratio info / Avg price N/A",
+    pnlCalculationError: "📈PnL +-% / Capital +-%📉 Calculation Error",
     entrySummary: "⏱️ Entry Summary:",
     entryInfoByTF: "• {tf}min → ✅ {percent}% / Avg {avg}",
     expectedCloseLong: "Expected close price: {price} ⬆️",
