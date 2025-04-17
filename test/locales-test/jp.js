8dfb8d07ded4fae3c4d020c689d2d0a2dac347dc
IS-Academy <choiis.academy@gmail.com>
2025-04-18 00:04:35 +0900

    Update jp.js

--- test/locales-test/jp.js
+++ test/locales-test/jp.js
@@ -1,18 +1,52 @@
+//✅👇 locales/jp.js
+
 module.exports = {
-  "labels": {
-    "symbol": "📌 シンボル",
-    "timeframe": "⏱️ 時間枠",
-    "price": "💲 価格",
-    "entryInfo": "📊 エントリー {entryCount}% / 平均価格 {entryAvg}",
-    "entrySummary": "⏱️ エントリー状況:",
-    "pnlLineProfit": "📈利益率 {pnl}% / 元本比 {capital}%",
-    "pnlLineLoss": "📉損失率 {pnl}% / 元本比 {capital}%",
-    "captured": "🕒 検出時間",
-    "weight": "🗝️ 比率: {weight}",
-    "leverage": "🎲 レバレッジ: {leverage}",
-    "disclaimer_full": "⚠️視点共有は常に任意です。\\n⚠️投資判断は自己責任でお願いします。"
+  symbols: {
+    Ready_showSup: "#🩵ロング準備 📈",
+    Ready_showRes: "#❤️ショート準備 📉",
+    Ready_isBigSup: "#🚀強ロング準備 📈",
+    Ready_isBigRes: "#🛸強ショート準備 📉",
+    Ready_exitLong: "#💲ロング利確準備 📈",
+    Ready_exitShort: "#💲ショート利確準備 📉",
+    showSup: "#🩵ロングエントリー 📈 視点共有🩵",
+    showRes: "#❤️ショートエントリー 📉 視点共有❤️",
+    isBigSup: "#🚀強ロングエントリー 📈 視点共有🚀",
+    isBigRes: "#🛸強ショートエントリー 📉 視点共有🛸",
+    exitLong: "#💰ロング利確 📈 視点共有💰",
+    exitShort: "#💰ショート利確 📉 視点共有💰"
   },
-  "symbols": {
-    "exitShort": "#💰ショート利確 📉 視点共有💰"
-  }
+  labels: {
+    symbol: "📌 シンボル",
+    timeframe: "⏱️ 時間枠",
+    timeframeUnit: "分",
+    price: "💲 価格",
+    entryInfo: "📊 エントリー {entryCount}% / 平均価格 {entryAvg}",
+    noEntryInfo: "📊 進入比率情報なし / 平均価格計算不可",
+    pnlCalculationError: "📈収益率 +-% / 元本対比 +-%📉 計算不可",
+    entrySummary: "⏱️ エントリー状況:",
+    entryInfoByTF: "• {tf}分 → ✅ {percent}% / 平均価格 {avg}",
+    expectedCloseLong: "想定決済価格: {price} ⬆️",
+    expectedCloseShort: "想定決済価格: {price} ⬇️",
+    pnlOnlyProfit: "📈損益 +{pnl}%",
+    pnlOnlyLoss: "📉損益 -{pnl}%",
+    roeOnlyProfit: "💹 ROE +{capital}%",
+    roeOnlyLoss: "💹 ROE -{capital}%",
+    pnlLineProfit: "📈利益率 {pnl}% / 元本比 {capital}%",
+    pnlLineLoss: "📉損失率 {pnl}% / 元本比 {capital}%",
+    pnlLineProfit: "📈利益率 {pnl}% / 元本比 {capital}%",
+    pnlLineLoss: "📉損失率 {pnl}% / 元本比 {capital}%",
+    noPnL: "📉収益率を計算できません",
+    entryLimitReachedLong: "⚠️ ロングポジションが飽和状態です。",
+    entryLimitReachedShort: "⚠️ ショートポジションが飽和状態です。",
+    captured: "🕒 検出時間",
+    weight: "🗝️ 比率: {weight}",
+    leverage: "🎲 レバレッジ: {leverage}",
+    pnl: "📈 損益: {value}%",
+    disclaimer_short: "⚠️視点は任意参加です。",
+    disclaimer_full: "⚠️視点共有は常に任意です。\n⚠️投資判断は自己責任でお願いします。"
+  },
+  days: {
+    0: "日", 1: "月", 2: "火", 3: "水", 4: "木", 5: "金", 6: "土"
+  },
+  am: "午前", pm: "午後", timezone: "Asia/Tokyo"
 };
