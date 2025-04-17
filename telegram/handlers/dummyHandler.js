ace6aefe2854e3b2254ab015660155597122e1df
IS-Academy <choiis.academy@gmail.com>
2025-04-17 20:35:11 +0900

    Update dummyHandler.js

--- telegram/handlers/dummyHandler.js
+++ telegram/handlers/dummyHandler.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const { updateLastDummyTime, getAdminMessageId } = require('../utils');
-const { sendBotStatus } = require('./commands/status');
+const { sendBotStatus } = require('../commands/status');
 const config = require('../config');
 
 router.post('/', async (req, res) => {
