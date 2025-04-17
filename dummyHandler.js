eff3380da69b7899b70860b69354d3d660320360
IS-Academy <choiis.academy@gmail.com>
2025-04-17 09:18:13 +0900

    Update dummyHandler.js

--- dummyHandler.js
+++ dummyHandler.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const { updateLastDummyTime } = require('./utils');
+const { updateLastDummyTime, getAdminMessageId } = require('./utils');
 const { sendBotStatus } = require('./commands/status');
 
 router.post('/', async (req, res) => {
