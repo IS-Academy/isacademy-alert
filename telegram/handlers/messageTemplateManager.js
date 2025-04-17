7baa2f13eaf8f172b2b786ddf18d5c7feda2e4f5
IS-Academy <choiis.academy@gmail.com>
2025-04-17 14:58:27 +0900

    Update messageTemplateManager.js

--- telegram/handlers/messageTemplateManager.js
+++ telegram/handlers/messageTemplateManager.js
@@ -2,7 +2,7 @@
 
 // 📦 모듈 및 템플릿 관련 함수 임포트
 const { getTemplate: getHeaderTemplate, formatDate, formatNumber, generateEntryInfo, calculatePnL, generatePnLLine, formatReadyLine } = require('../MessageTemplates');
-const { translations } = require('../../lang');
+const { translations } = require('../lang');
 const lang = require("../lang");
 const langManager = require('../langConfigManager');
 const { getEntryInfo } = require('../entryManager');
