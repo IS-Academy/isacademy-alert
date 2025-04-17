0705dc04b729dc1928c1820fae15c78f7529cae3
IS-Academy <choiis.academy@gmail.com>
2025-04-17 14:27:22 +0900

    Update testTemplateHandler.js

--- telegram/handlers/testTemplateHandler.js
+++ telegram/handlers/testTemplateHandler.js
@@ -1,5 +1,5 @@
 const webhookHandler = require('../webhookHandler');
-const config = require('../config');
+const config = require('../../config');
 
 module.exports = async function testTemplate(type) {
   const testWebhookData = {
