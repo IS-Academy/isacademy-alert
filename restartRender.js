adf0ce0a8ba7ceba0c6d0158514b3885cf34e1b4
IS-Academy <choiis.academy@gmail.com>
2025-04-18 10:23:56 +0900

    Update restartRender.js

--- restartRender.js
+++ restartRender.js
@@ -1,11 +1,14 @@
+//✅👇 restartRender.js
+
 require('dotenv').config();
 const axios = require('axios');
 
 const SERVER_URL = process.env.SERVER_URL;
+const RESTART_TOKEN = process.env.RESTART_TOKEN;
 
 async function triggerRestart() {
   try {
-    const res = await axios.get(`${SERVER_URL}/restart`);
+    const res = await axios.get(`${SERVER_URL}/restart?token=${RESTART_TOKEN}`);
     console.log('♻️ 서버 재시작 요청 완료:', res.data);
   } catch (err) {
     console.error('❌ 재시작 요청 실패:', err.message);
